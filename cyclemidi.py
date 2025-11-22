0f629f5f91041c55a2267c81d549c4e6c33fcd2e
Chris Cutler <1834936+ctcutler@users.noreply.github.com>
2025-11-22 15:34:46 -0500

    Convert "private" methods to module level functions

--- cyclemidi.py
+++ cyclemidi.py
@@ -42,6 +42,97 @@ class Note:
 Voice = list[Union[Note, Rest]]
 CycleString = tuple[CycleStringType, str]
 
+###
+# Note: the public interface (`Cycles`) is object-orented and "fluent" but the actual processing is done
+# in a series of "pure" functions defined at the module level.
+###
+
+
+def tokenize(cycle_string: str) -> list[str]:
+    tokens = []
+    cur_token = ""
+    for c in cycle_string:
+        if c not in whitespace + "[]":
+            cur_token += c
+        else:
+            if cur_token != "":
+                tokens.append(cur_token)
+                cur_token = ""
+            if c in "[]":
+                tokens.append(c)
+
+    if cur_token != "":
+        tokens.append(cur_token)
+
+    return tokens
+
+
+def add_cycle_to_tree(tokens: list[str], tree: TreeNode) -> int:
+    """
+    Adds the tokens of a single (potentially nested) cycle into an
+    existing tree.
+    """
+    i = 0
+    token_count = len(tokens)
+    while i < token_count:
+        token = tokens[i]
+        if token == "[":  # this is subtree's open brackent
+            subtree = TreeNode([])
+            tokens_consumed = add_cycle_to_tree(tokens[i + 1 :], subtree)
+            tree.children.append(subtree)
+            i += tokens_consumed + 1
+        elif token == "]":  # this  is tree's close bracket
+            return i
+        else:
+            tree.children.append(token)
+            i += 1
+
+    raise Exception(f"{tokens} has mismatched brackets")
+
+
+def parse_cycles(cycle_string: str) -> list[str]:
+    """
+    So:
+    - treeify
+    - count elements at current level
+    - for each element at current level:
+      - if leaf:
+        set length
+      - if node:
+        recurse
+    """
+    cycle_tree = TreeNode([])
+
+    # the top level is a list of one or more cycles that we need to split up
+    # and parse into the same tree
+    cycles = re.split(r"(?<=\])\s*(?=\[)", cycle_string)
+    for cycle in cycles:
+        tokens = tokenize(cycle)
+        add_cycle_to_tree(tokens, cycle_tree)
+
+    print(cycle_tree)
+
+    return []
+
+
+def parse_cycle_strings(cycle_strings: list[CycleString]) -> list[Voice]:
+    voices: list[Voice] = [[]]
+    base_voice_idx = 0
+    for cycle_string_type, cycle_string in cycle_strings:
+        if cycle_string_type == CycleStringType.STACK:
+            voices.append([])
+            base_voice_idx = len(voices) - 1
+        elif cycle_string_type == CycleStringType.NOTES:
+            parse_cycles(cycle_string)
+            # split cycles into notes
+            # append notes to voice(s)
+
+    return voices
+
+
+def generate_midi(voices: list[Voice]) -> None:
+    pass
+
 
 class Cycles:
     def __init__(self) -> None:
@@ -49,88 +140,6 @@ class Cycles:
         self.midi_file = None
         self.total_secs = None
         self.config = Config()
-
-    # "private" methods
-    def _tokenize(self, cycle_string: str) -> list[str]:
-        tokens = []
-        cur_token = ""
-        for c in cycle_string:
-            if c not in whitespace + "[]":
-                cur_token += c
-            else:
-                if cur_token != "":
-                    tokens.append(cur_token)
-                    cur_token = ""
-                if c in "[]":
-                    tokens.append(c)
-
-        if cur_token != "":
-            tokens.append(cur_token)
-
-        return tokens
-
-    def _add_cycle_to_tree(self, tokens: list[str], tree: TreeNode) -> int:
-        """
-        Adds the tokens of a single (potentially nested) cycle into an
-        existing tree.
-        """
-        i = 0
-        token_count = len(tokens)
-        while i < token_count:
-            token = tokens[i]
-            if token == "[":  # this is subtree's open brackent
-                subtree = TreeNode([])
-                tokens_consumed = self._add_cycle_to_tree(tokens[i + 1 :], subtree)
-                tree.children.append(subtree)
-                i += tokens_consumed + 1
-            elif token == "]":  # this  is tree's close bracket
-                return i
-            else:
-                tree.children.append(token)
-                i += 1
-
-        raise Exception(f"{tokens} has mismatched brackets")
-
-    def _parse_cycles(self, cycle_string: str) -> list[str]:
-        """
-        So:
-        - treeify
-        - count elements at current level
-        - for each element at current level:
-          - if leaf:
-            set length
-          - if node:
-            recurse
-        """
-        cycle_tree = TreeNode([])
-
-        # the top level is a list of one or more cycles that we need to split up
-        # and parse into the same tree
-        cycles = re.split(r"(?<=\])\s*(?=\[)", cycle_string)
-        for cycle in cycles:
-            tokens = self._tokenize(cycle)
-            self._add_cycle_to_tree(tokens, cycle_tree)
-
-        print(cycle_tree)
-
-        return []
-
-    def _parse_cycle_strings(self, cycle_strings: list[CycleString]) -> list[Voice]:
-        voices: list[Voice] = [[]]
-        base_voice_idx = 0
-        for cycle_string_type, cycle_string in cycle_strings:
-            if cycle_string_type == CycleStringType.STACK:
-                voices.append([])
-                base_voice_idx = len(voices) - 1
-            elif cycle_string_type == CycleStringType.NOTES:
-                self._parse_cycles(cycle_string)
-                # split cycles into notes
-                # append notes to voice(s)
-
-        return voices
-
-    def _generate_midi(self, voices: list[Voice]) -> None:
-        pass
 
     # "public" methods
     def notes(self, cycle_string: str) -> Cycles:
@@ -162,8 +171,8 @@ class Cycles:
         return self
 
     def midi(self) -> Cycles:
-        voices = self._parse_cycle_strings(self.cycle_strings)
-        self._generate_midi(voices)
+        voices = parse_cycle_strings(self.cycle_strings)
+        generate_midi(voices)
 
         return self
 
